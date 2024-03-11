2a93ee125f3f5ec569f309bd261143c09f33100a
ikachaeva <irenkachaeva@gmail.com>
2024-03-11 21:51:12 +0300

    структура проекта

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,8 +21,35 @@ def test_get_sorted_list():
     assert get_sorted_list(data_for_test,2)[0]["date"] == '2019-08-26T10:50:58.294041'
     assert len(get_sorted_list(data_for_test, 2)) == 2
 
+@pytest.fixture
+def deposit():
+    return {
+        "description": "Открытие вклада",
+        "from": "",
+        "to": "1234",
+        "date": "2022-01-01",
+        "operationAmount": {
+            "amount": 1000,
+            "currency": {"name": "USD"}
+        }
+    }
 
+@pytest.fixture
+def transfer():
+    return {
+        "description": "Перевод",
+        "from": "Счет1234567890123456",
+        "to": "1234",
+        "date": "2022-01-01",
+        "operationAmount": {
+            "amount": 500,
+            "currency": {"name": "RUB"}
+        }
+    }
 
+def test_format_operation(deposit, transfer):
+    assert format_operation(deposit) == "01.01.2022 Открытие вклада\n   -> Счет ** 1234\n1000 USD\n"
+    assert format_operation(transfer) == "01.01.2022 Перевод\nСчет ** 1234  -> Счет ** 1234\n500 RUB\n"
 
 
 
